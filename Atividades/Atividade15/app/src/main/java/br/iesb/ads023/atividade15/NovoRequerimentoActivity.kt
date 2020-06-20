7d6b2908ec778f3c56c12f31812ca0c767cb07e7
Paulo <pauloc.alves@gmail.com>
2020-06-19 22:32:26 -0300

    Enviando Atividade

---
+++ Atividades/Atividade15/app/src/main/java/br/iesb/ads023/atividade15/NovoRequerimentoActivity.kt
@@ -0,0 +1,12 @@
+package br.iesb.ads023.atividade15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NovoRequerimentoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_requerimento)
+        this.title = "Requerimento"
+    }//end onCreate
+}//end Class
