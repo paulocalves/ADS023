1e53b32489a62c7c01fa77290ed14c0a357d3613
Paulo <pauloc.alves@gmail.com>
2020-06-19 22:05:07 -0300

    Envio de Atividade

--- Atividades/Atividade11/app/src/main/java/br/iesb/ads023/atividade11/MainActivity.kt
+++ Atividades/Atividade11/app/src/main/java/br/iesb/ads023/atividade11/MainActivity.kt
@@ -1,12 +1,40 @@
 package br.iesb.ads023.atividade11
 
-import androidx.appcompat.app.AppCompatActivity
+//import android.app.Activity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        this.title = "Atividade 11"
+        println("onCreate disparado...")
+        setContentView(R.layout.activity_main);
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart disparado...")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume disparado...")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause disparado...")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop disparado...")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy disparado...")
     }
 }
